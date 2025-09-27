427892fa2c53520697bc1788da3513738b6fb50c
tmampa <tmampa94@gmail.com>
2025-09-27 17:43:16 +0200

    fix adding of bootstrap

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,35 +1,24 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="navbar-start">
-      <h6 class="">School Textbook Tracking Application</h6>
-    </div>
-    <div class="navbar-end">
-      <div class="dropdown dropdown-left">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-        >
-          <li><a href="/">Homepage</a></li>
-          <li><a href="/books">Manage books</a></li>
-          <li><a href="/students">Manage students</a></li>
-        </ul>
-      </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">School Textbook Tracking Application</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/books">Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/students">Students</a>
+        </li>
+      </ul>
     </div>
   </div>
+</nav>
+
 </template>
