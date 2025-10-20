fc44b4f585d3d0ca92601ee5681f33c60fb83518
tmampa <tmampa94@gmail.com>
2025-10-20 18:09:56 +0200

    update ip address

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -28,7 +28,7 @@ export const useAuth = () => {
   const isAuthenticated = computed(() => !!user.value);
   const isLoading = ref(false);
 
-  const basePath: string = "http://localhost:8080/api/auth";
+  const basePath: string = "http://172.179.240.23:8080/api/auth";
 
   // Login with API call
   const login = async (credentials: { email: string; password: string }) => {
